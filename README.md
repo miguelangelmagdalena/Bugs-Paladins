96c4075df384ef21eac0c2f2904df05cf517da86
musthagon <miguelangelmagdalena@gmail.com>
2017-01-29 19:57:59 -0400

    Muestra de algunos datos del csv del lab_0, sin gráficos y sin análisis

--- README.md
+++ README.md
@@ -1,4 +1,10 @@
 # lab_1
 Lab_1_Ciencia_de_datos
 
-probando
+Quería extraer los datos del csv del laboratorio anterior
+y realizar un histograma y el diagrama de dispersión, pero
+tengo algunos errores para hacer plot para hacer diagramas
+entonces solo extraje algunos datos del csv y los mostré 
+por consola
+
+Miguel Magdalena
--- lab_1.py
+++ lab_1.py
@@ -1,5 +1,8 @@
 import csv
-import plotly
+from plotly.offline import download_plotlyjs, init_notebook_mode, plot, iplot
+from plotly.graph_objs import *
+#init_notebook_mode()
+import numpy as np
 
 #Abrimos la hoja de calculo con la trabajaremos
 with open('student-mat.csv') as csvfile:
@@ -7,7 +10,7 @@ with open('student-mat.csv') as csvfile:
 	
 	ages	= [] 
 	g1		= [] 
- 	g2		= []  
+	g2		= []  
 	
 	for row in readCSV:
 		age = row[2] 
@@ -17,11 +20,27 @@ with open('student-mat.csv') as csvfile:
 		ages.append(age)
 		g1.append(g1_data)
 		g2.append(g1_data)
-	
-	print (ages)
-	print ("\n\n")
-	print (g1)
-	print ("\n\n")
-	print (g2) 
 
-print "hola"
+#mostramos datos del csv
+print ("Edades de alumnos y resultados de examenes g1 y g2")
+print (ages)
+print ("\n\n")
+print (g1)
+print ("\n\n")
+print (g2) 
+
+
+#hacemos algunos calculos con la data
+'''
+n= 1000
+random_x = np.random.randn(n)
+random_y = np.random.randn(n)
+
+trace = Scatter(
+	x = random_x,
+	y = random_y,
+	mode = 'markers'
+)
+data = [trace]
+iplot(data)
+'''
